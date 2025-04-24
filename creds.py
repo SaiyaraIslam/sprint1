fb4052bdace2d139d8c2affeb0b7e8d9e15f7cc4
SaiyaraIslam <sislam24@cougarnet.uh.edu>
2025-04-23 22:47:01 -0500

    sp2 commit

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 # creds file with login info
-DB_HOST = "cis2368db.cw9atuop3ryf.us-east-1.rds.amazonaws.com"
-DB_USER = "saiyaracis"
-DB_PASSWORD = "Cis2368db"
+DB_HOST = "db2368.cc1acqj6ueqy.us-east-1.rds.amazonaws.com"
+DB_USER = "Saiyara1"
+DB_PASSWORD = "Cis2368spring"
 DB_NAME = "library_db"
